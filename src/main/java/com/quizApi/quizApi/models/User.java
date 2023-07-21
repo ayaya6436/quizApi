a289be4841db71d872a28ec743dec2fdb3433bef
ayaya <ayaya6436@gmail.com>
2023-07-21 09:11:15 +0000

    modele user

--- src/main/java/com/quizApi/quizApi/models/User.java
+++ src/main/java/com/quizApi/quizApi/models/User.java
@@ -1,5 +1,31 @@
 package com.quizApi.quizApi.models;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+
+
+ @Entity
+ @Table(name = "user")
+ @Setter
+ @Getter
+ @NoArgsConstructor
 public class User {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length=50)
+    private String nom;
+
+    @Column(length=50)
+    private String prenom;
+
+    @Column(length=50)
+    private String username;
+
+    @Column(length=150)
+    private String password;
     
 }
